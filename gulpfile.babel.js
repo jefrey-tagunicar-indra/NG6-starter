37501729c911439aa08cde26895b96b287e796de
jefrey-tagunicar-indra <jefrey.tagunicar.ext@flydubai.com>
2016-04-13 15:31:34 +0800

    added bower and serve node_modules and bower_components in gulp file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -75,7 +75,12 @@ gulp.task('serve', () => {
   serve({
     port: process.env.PORT || 3000,
     open: false,
-    server: {baseDir: root},
+    server: {baseDir: root,
+    ,routes: {
+        '/bower_components': 'bower_components',
+        '/node_modules':'node_modules'
+      }
+    },
     middleware: [
       historyApiFallback(),
       webpackDevMiddelware(compiler, {
